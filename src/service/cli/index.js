38ee3da4822c653669e426b6c5f9411e6807aaa6
Anastasia <izotova@sminex.com>
2021-09-02 18:42:25 +0300

    refactor cli index

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,4 +1,16 @@
 'use strict';
 
-const {cli} = require(`./src/service/cli`);
+const help = require(`./help`);
+const generate = require(`./generate`);
+const version = require(`./version`);
+
+const Cli = {
+  [generate.name]: generate,
+  [help.name]: help,
+  [version.name]: version,
+};
+
+module.exports = {
+  Cli,
+};
 
