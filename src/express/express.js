ba22c0724b1c9517ccbbc6589dd16abe83c25480
Anastasia <ana.izotova@gmail.com>
2021-09-25 23:45:31 +0300

    add static middleware

--- src/express/express.js
+++ src/express/express.js
@@ -1,15 +1,18 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const articlesRoutes = require(`./routes/articles-routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
