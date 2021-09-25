1d66769d0a0548e3c53217d7e748d05b853212d7
Anastasia <ana.izotova@gmail.com>
2021-09-25 23:50:05 +0300

    configure express to work with pug

--- src/express/express.js
+++ src/express/express.js
@@ -12,9 +12,15 @@ const PUBLIC_DIR = `public`;
 
 const app = express();
 
+app.listen(DEFAULT_PORT);
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
 
-app.listen(DEFAULT_PORT);
+
