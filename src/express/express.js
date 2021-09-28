6f112176347f85c68105cee86c6cb4d51ed90f59
Anastasia <izotova@sminex.com>
2021-09-28 18:45:45 +0300

    add error handling

--- src/express/express.js
+++ src/express/express.js
@@ -7,20 +7,24 @@ const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const articlesRoutes = require(`./routes/articles-routes`);
 
+const {HttpCode} = require(`../const`);
+
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
 
 const app = express();
 
-app.listen(DEFAULT_PORT);
-
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+// app.use(express.static(path.join(__dirname + '/../public')));
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
 
+app.use((req, res) => res.status(HttpCode.NOT_FOUND).render(`errors/404`));
+app.use((req, res) => res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`errors/500`));
 
+app.listen(DEFAULT_PORT);
